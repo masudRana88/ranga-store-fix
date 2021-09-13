e66054bf3fec8f7d19dba78d1561e00dc4423d50
masudRana88 <mahimuddin88@gmail.com>
2021-09-13 13:48:06 +0600

    total tax bug fix

--- js/app.js
+++ js/app.js
@@ -28,18 +28,17 @@ const showProducts = (products) => {
   }
 };
 let count = 0;
+// add to cart handeler
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
-
+// get value form any html element (with out inpute)
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
   const converted = parseFloat(element);
-  converted.toFixed(2);
   return converted;
 };
 
@@ -47,34 +46,39 @@ const getInputValue = (id) => {
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
-  convertPrice.toFixed(2);
   const total = convertedOldPrice + convertPrice;
   // document.getElementById(id).innerText = Math.round(total);
-  document.getElementById(id).innerText = total;
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  // document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
+  console.log(priceConverted)
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
-    setInnerText("total-tax", priceConverted * 0.2);
+    setInnerText("total-tax", (priceConverted * 0.2).toFixed(2));
   }
   if (priceConverted > 400) {
     setInnerText("delivery-charge", 50);
-    setInnerText("total-tax", priceConverted * 0.3);
+    setInnerText("total-tax", (priceConverted * 0.3).toFixed(2));
   }
   if (priceConverted > 500) {
     setInnerText("delivery-charge", 60);
-    setInnerText("total-tax", priceConverted * 0.4);
+    setInnerText("total-tax", (priceConverted * 0.4).toFixed(2));
   }
 };
-
+// set InnerText
+// const setInnerText = (id, value) =>{
+//   const element = getElementById(id);
+//   element.innerText = value;
+// }
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
